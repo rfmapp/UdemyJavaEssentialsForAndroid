2188b5fd78ea7d137ffaca8a56a74345b8dd9667
Rafael Marques <rfmapp@gmail.com>
2015-10-29 17:09:43 -0300

    Introdução aos tipos de dados primitivos. Começando com inteiros.

---
+++ src/pkg1PrimitiveDataTypes/app1JavaIntOperations.java
@@ -0,0 +1,18 @@
+package pkg1PrimitiveDataTypes;
+
+/**
+ * Created by rafael on 29/10/15.
+ */
+public class app1JavaIntOperations {
+
+    public static void main(String [] args){
+        // Iremos exbir algumas operações com inteiros. Mas primeiro iremos simplesmente exibir um inteiro.
+        System.out.println("Esse é o inteiro " + (7));
+        // Algumas operações básicas com inteiros.
+        System.out.println("A soma de 7 + 3 é " + (7 + 3));
+        System.out.println("A subtração de 7 - 3 é " + (7 - 3));
+        System.out.println("O produto de 7 * 3 é " + (7 * 3));
+        System.out.println("A divisão de 7 / 3 é " + (7 / 3));
+        System.out.println("O resto da divisão de 7 % 3 é " + (7 % 3));
+    }
+}
