0b1ffe016c7576860d9aca3b1b8ed6def067a778
Rafael Marques <rfmapp@gmail.com>
2015-10-29 16:32:51 -0300

    Primeiro programa em Java, o famoso "HelloWorld!"

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by rafael on 29/10/15.
+ */
+public class HelloWorld {
+    // Método principal de uma classe Java, necessário para executar o programa.
+    public static void main(String [] args){
+        System.out.println("Olá Mundo! Meu primeiro programa em Java!!");
+    }
+}
